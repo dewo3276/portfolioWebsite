d838ab376e94227ee89f885d44702b011687b48d
Destin Woods <dewo3276@colorado.edu>
2021-07-29 15:28:40 -0600

    version 1.25

--- js/button.js
+++ js/button.js
@@ -2,32 +2,15 @@ var projectsInFile = ['.project1', '.project2', '.project3', '.project4', '.proj
 var pos = document.getElementById("workSection").offsetTop;
 
 function enlarge(className, type, index) {
-  switch (type) {
-    case 0:
-      gsap.set(className, {
-        transformOrigin: "0 0"
-      });
-      break;
-    case 1:
-      gsap.set(className, {
-        transformOrigin: "50% 0"
-      });
-      break;
-    case 2:
-      gsap.set(className, {
-        transformOrigin: "100% 0"
-      });
-      break;
-    default:
-      gsap.set(className, {
-        transformOrigin: "0 0"
-      });
-  }
+  var pos2 = document.getElementsByClassName(className.substring(1))[0].offsetTop-(window.innerHeight/2);
+
+  gsap.set(className, {transformOrigin:"0 50"});
   gsap.timeline()
-    .to(window, {scrollTo: pos, duration:.5})
-    .to(className, {scaleX: "313%", scaleY: "265%", zIndex: 1, duration: 1})
+    .to(window, {scrollTo: pos2, duration:.25})
+    .to(className, {scaleX: "150%", scaleY: "150%", zIndex: 1, duration: 1})
     .to(className, {opacity: 0},'-=10%')
-    .to('#WorkContent' + (index + 1), {display: "inline-grid", opacity: 1, duration: 1},'-=5%');
+    .to(window, {scrollTo: pos, duration:.5})
+    .to('#WorkContent' + (index + 1), {display: "grid", opacity: 1, duration: 1},'-=5%');
 
   for (let i = 0; i < projectsInFile.length; i++) {
     if (i != index) {
@@ -37,11 +20,13 @@ function enlarge(className, type, index) {
 }
 
 function shrink(className, index) {
+  var pos2 = document.getElementsByClassName(className.substring(1))[0].offsetTop;
+
   gsap.timeline()
-    .to(window, {scrollTo: pos, duration:.5})
     .to('#WorkContent' + (index + 1), {opacity: 0, display: "none"})
     .to(className, {opacity: 1},'-=20%')
-    .to(className, {scaleX: "100%", scaleY: "100%", x: "0", backgroundColor: "none", zIndex: 0, duration: 1});
+    .to(className, {scaleX: "100%", scaleY: "100%", backgroundColor: "none", zIndex: 0, duration: .2})
+    .to(window, {scrollTo: pos, duration:.5})
 
   for (let i = 0; i < projectsInFile.length; i++) {
     if(index!=i)
