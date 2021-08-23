4211d6cb24b1d3af3483bc4f7d4b3ce6e2353a15
Destin Woods <dewo3276@colorado.edu>
2021-08-23 15:06:02 -0600

    version 2.2 mergers

--- js/button.js
+++ js/button.js
@@ -1,5 +1,4 @@
 var projectsInFile = ['.project1', '.project2', '.project3', '.project4', '.project5', '.project6', '.project7', '.project8', '.project9', '.project10', '.project11', '.project12', '.project13', '.project14', '.project15', '.project16'];
-var pos = document.getElementById("workSection").offsetTop;
 
 function enlarge(className, type, index) {
   var pos2 = document.getElementsByClassName(className.substring(1))[0].offsetTop-(window.innerHeight*.1);
@@ -17,8 +16,10 @@ function enlarge(className, type, index) {
     .to(window, {scrollTo: pos2, duration:.25})
     .to(className, {scaleX: "150%", scaleY: "150%", zIndex: 1, duration: 1})
     .to(className, {opacity: 0},'-=10%')
-    .to(window, {scrollTo: pos, duration:.5})
-    .to('#WorkContent' + (index + 1), {display: "block", opacity: 1, duration: 1},'-=5%');
+    .to('#WorkContent' + (index + 1), {display: "block", opacity: 1, duration: 1},'-=5%')
+    .to('#workDiv',{display: "none"});
+  var pos = document.getElementById('header').offsetTop;
+    gsap.to(window, {scrollTo: pos, duration:.5},'>');
 
   for (let i = 0; i < projectsInFile.length; i++) {
     if (i != index) {
@@ -28,8 +29,9 @@ function enlarge(className, type, index) {
 }
 
 function shrink(className, index) {
-  var pos2 = document.getElementsByClassName(className.substring(1))[0].offsetTop-(window.innerHeight*.1);
 
+  document.getElementById('workDiv').style.display="grid";
+    var pos2 = document.getElementsByClassName(className.substring(1))[0].offsetTop-(window.innerHeight*.1);
   gsap.timeline()
     .to(window, {scrollTo: pos2, duration:.5})
     .to('#WorkContent' + (index + 1), {opacity: 0, display: "none"},'-=10%')
--- js/navBarButton.js
+++ js/navBarButton.js
@@ -1,5 +1,10 @@
 function navigationContoller(id){
   var pos = document.getElementById(id).offsetTop;
 
+  if(id=='header')
+  {
+    var pos = document.getElementById(id).offsetTop-(window.innerHeight*.1)
+  }
+
   gsap.to(window, {scrollTo: pos, delay: .5, duration: 1});
 }
