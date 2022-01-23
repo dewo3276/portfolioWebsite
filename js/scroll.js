0abf5f591110ff996010205a2a3df2ab2a709993
Destin Woods <dewo3276@colorado.edu>
2022-01-22 21:28:18 -0700

    needed for re-re-vamp branch

--- js/scroll.js
+++ js/scroll.js
@@ -1,51 +1,72 @@
 ScrollTrigger.create({
-  trigger:".main",
-  start: "5%",
-  end: "bottom+=10%",
-  onToggle: function () {
+  trigger: ".main",
+  start: "10%",
+  end: "bottom-=50%",
+  onToggle: function() {
     var element = document.getElementsByTagName("NAV")[0];
-    element.style.display="none";
-    gsap.to(element,{opacity: 0, delay: .25, duration: .5})
+    element.style.display = "none";
+    gsap.to(element, {
+      opacity: 0,
+      delay: .25,
+      duration: .5
+    })
   },
   onLeave: function() {
-      var element = document.getElementsByTagName("NAV")[0];
-      element.style.display="flex";
-      gsap.to(element,{opacity: 1, delay: .25 ,duration: .5});
-}
+    var element = document.getElementsByTagName("NAV")[0];
+    element.style.display = "flex";
+    gsap.to(element, {
+      opacity: 1,
+      delay: .25,
+      duration: .5
+    });
+  }
 });
+
+window.addEventListener('scroll', function() {
+  let value = window.scrollY;
+  let about = document.getElementById('aboutMeSection');
+  let coolPic = document.getElementById('aboutMepic');
+  let path1 = document.getElementById('path1');
+  let path2 = document.getElementById('path2');
+  let path3 = document.getElementById('path3');
+  let path4 = document.getElementById('path4');
+  if (value < 800) {
+    about.style.left = (value - 500) + value * -.45 + 'px';
+  }
+  path1.style.strokeDashoffset = value * -2 + 'px';
+  path2.style.strokeDashoffset = value * 2 + 'px';
+  path3.style.strokeDashoffset = value * 2 + 'px';
+  path4.style.strokeDashoffset = value * 2 + 'px';
+  if (value > 200 && value < 400) {
+    gsap.to('#welcomeBanner', {
+      transformOrigin: "0 0",
+      scaleX: value * .005,
+      scaleY: value * .005,
+      opacity: value * (1 / value * 1.25)
+    })
+  } else if (value <= 200) {
+    gsap.to('#welcomeBanner', {
+      transformOrigin: "0 0",
+      scaleX: 1,
+      scaleY: 1
+    })
+  } else if (value >= 400 && value<600) {
+    gsap.to('#welcomeBanner', {
+      opacity: value * (1 / value * .25)
+    });
+    a
+  } else {
+    gsap.to('#welcomeBanner', {
+      opacity: 0
+    })
+  }
+})
 
 gsap.to('#CTDworkDiv', {
   xPercent: -548,
   ease: "none",
   scrollTrigger: {
     trigger: "#CTDworkDiv",
-    onEnter: function() {
-      var element2 = document.getElementById("header");
-      var element = document.getElementsByTagName("NAV")[0];
-      element2.style.position="sticky";
-      gsap.to(element,{y:"-60",duration:.5});
-      gsap.to(element2,{y:"-15",duration:.5},"-=105%");
-    },
-    onLeaveBack: function () {
-      var element2 = document.getElementById("header");
-      var element = document.getElementsByTagName("NAV")[0];
-      gsap.to(element,{y:"0",duration:.5});
-      element2.style.position="relative";
-      gsap.to(element2,{y:"0",duration:.75});
-    },
-    snap: 1/5,
-    start:"top 5%",
-    end:"bottom",
-    scrub: .15,
-    pin: true
-  }
-})
-
-gsap.to('#workDivTheater', {
-  xPercent: -548,
-  ease: "none",
-  scrollTrigger: {
-    trigger: "#workDivTheater",
     onEnter: function() {
       var element2 = document.getElementById("header");
       var element = document.getElementsByTagName("NAV")[0];
